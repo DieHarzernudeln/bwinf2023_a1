da187f81d5a3dadbbbdd509404fd09111a04ce1e
Konsl <82901383+Konsl@users.noreply.github.com>
2023-09-07 21:39:45 +0200

    trap creation hat jz eigene methode

--- arukone.py
+++ arukone.py
@@ -3,62 +3,79 @@
 import math
 import random
 
-# [[y, x], value]
-solver_trap_pattern = [
-    [[0, 0], 1],
-    [[1, 3], 1],
-    [[0, 3], 2],
-    [[2, 1], 2]
-]
+class SchemaPunkt:
+    def __init__(self, x, y, wert) -> None:
+        self.x = x
+        self.y = y
+        self.wert = wert
 
-trap_pattern_size = 4
+# (breite_hoehe, ist_links, ist_oben, [(x, y, wert), ...])
+def erstelle_falle(fenster_groesse: int) -> tuple[int, bool, bool, list[SchemaPunkt]]:
+    schema_breite_hoehe = 4
+    schema = [
+        SchemaPunkt(0, 0, 1),
+        SchemaPunkt(3, 1, 1),
+        SchemaPunkt(3, 0, 2),
+        SchemaPunkt(1, 2, 2)
+    ]
 
-# Ecke für Trap
-trap_top = True
-trap_left = False
+    ist_oben = True
+    ist_links = False
 
-def erstelle_arukone(n: int) -> list[list[int]]:
-    global trap_top, trap_left
-
-    arukone = [[0 for _ in range(n)] for _ in range(n)]
-    paare = math.ceil(n / 2)
-
-    # Spiegeln in y-Richtung
-    if random.getrandbits(1):
-        trap_top = not trap_top
-
-        for pattern_entry in solver_trap_pattern:
-            pattern_entry[0][0] = 3 - pattern_entry[0][0]
-            
     # Spiegeln in x-Richtung
     if random.getrandbits(1):
-        trap_left = not trap_left
+        ist_links = not ist_links
 
-        for pattern_entry in solver_trap_pattern:
-            pattern_entry[0][1] = 3 - pattern_entry[0][1]
+        for eintrag in schema:
+            eintrag.x = (schema_breite_hoehe - 1) - eintrag.x
+            
+    # Spiegeln in y-Richtung
+    if random.getrandbits(1):
+        ist_oben = not ist_oben
+
+        for eintrag in schema:
+            eintrag.y = (schema_breite_hoehe - 1) - eintrag.y
             
     # Drehen um 90°
     if random.getrandbits(1):
-        old_top = trap_top
-        trap_top = trap_left
-        trap_left = not old_top
+        temp = ist_oben
+        ist_oben = ist_links
+        ist_links = not temp
 
-        for pattern_entry in solver_trap_pattern:
-            temp = pattern_entry[0][0]
-            pattern_entry[0][0] = pattern_entry[0][1]
-            pattern_entry[0][1] = 3 - temp
+        for eintrag in schema:
+            temp = eintrag.y
+            eintrag.y = eintrag.x
+            eintrag.x = (schema_breite_hoehe - 1) - temp
 
-    trap_pattern_x = 0 if trap_left else n - trap_pattern_size
-    trap_pattern_y = 0 if trap_top else n - trap_pattern_size
+    # an Position bewegen
 
-    for pattern_entry in solver_trap_pattern:
-        arukone[trap_pattern_y + pattern_entry[0][0]][trap_pattern_x + pattern_entry[0][1]] = pattern_entry[1]
+    x_position_falle = 0 if ist_links else fenster_groesse - schema_breite_hoehe
+    y_position_falle = 0 if ist_oben else fenster_groesse - schema_breite_hoehe
+
+    for eintrag in schema:
+        eintrag.x += x_position_falle
+        eintrag.y += y_position_falle
+
+    return (schema_breite_hoehe, ist_links, ist_oben, schema)
+
+def erstelle_arukone(n: int) -> list[list[int]]:
+    arukone = [[0 for _ in range(n)] for _ in range(n)]
+    paare = math.ceil(n / 2)
+
+    # Falle übertragen
+
+    falle_groesse, falle_links, falle_oben, falle_schema = erstelle_falle(n)
+
+    for eintrag in falle_schema:
+        arukone[eintrag.y][eintrag.x] = eintrag.wert
+
+    # mit restlichen Paaren auffüllen
 
     for i in range(3, paare + 1):
         y_position_start = random.randint(0, n - 3)
         y_position_ende = random.randint(y_position_start + 2, n - 1)
 
-        x_position = (trap_pattern_size if trap_left else 0) + i - 3
+        x_position = (falle_groesse if falle_links else 0) + i - 3
 
         arukone[y_position_start][x_position] = i
         arukone[y_position_ende][x_position] = i
@@ -67,7 +84,6 @@ def erstelle_arukone(n: int) -> list[list[int]]:
 
 
 # Testen
-
 
 def arukone_anzeigen(arukone: list[list[int]]):
     n = len(arukone)
